bea2fc3a0fc76f7c392555e657f673a73a73f476
jirin <jirinovy@outlook.com>
2021-06-25 00:05:13 +0100

    Delete old QR codes every time the app is run.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, flash, session
 from flask_bootstrap import Bootstrap
-import os
+import os, shutil
 from werkzeug.utils import secure_filename
 from forms import GenerateQrForm
 import qrcode
@@ -15,10 +15,24 @@ def generate_qr(data="https://www.youtube.com/channel/UCFOVnO-D9CeAm3GJqP5m6qg")
     image = qr_generator.make_image(fill_color="black", back_color="white")
     # generated image is a PIL object
     current_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    image_path = f"static/img/qr_code_{current_date}.png"
+    image_path = f"static/img/qr_codes/qr_code_{current_date}.png"
     image.save(image_path)
 
     return image_path
+
+
+# Deletes all previous QR Codes ----------------------------------------------------------------------------------------
+def delete_qr_codes():
+    directory = "static/img/qr_codes/"
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
 
 
 # Main code ------------------------------------------------------------------------------------------------------------
@@ -32,6 +46,8 @@ def home():
     qr_form = GenerateQrForm()
 
     if qr_form.validate_on_submit():
+        # Remove all previous QR codes
+        delete_qr_codes()
         # Remove all previous flash messages
         session.pop("_flashes", None)
         # Get data to encode -------------------------------------------------------------------------------------------
